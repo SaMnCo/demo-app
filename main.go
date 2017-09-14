0c98dbbf8a4c902cfa3df574cd870a3b7fc4fc5d
Samuel Cozannet <samnco@gmail.com>
2017-09-14 17:08:14 +0100

    new app

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello GCP CD this is version 8!")
+	io.WriteString(w, "Hello GCP CD this is version 9!")
 }
 
 func main() {
